2d5d570395371e4467cf74789400c670bb8d1d9a
Guillermo Martin <gscalica@gmail.com>
2024-08-19 21:17:17 -0700

    Delete 'manifest.json', properly connect form inputs to form state

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,23 @@
+import { useState } from "react";
+
 function LoginForm() {
+  // ---------- State ----------
+  const [formInputs, setFormInputs] = useState({});
+  
+  // ---------- Functions ----------
+  // handling multiple inputs:  https://stackoverflow.com/questions/63710791/react-hooks-handle-multiple-inputs
+  const handleInputs = (event) => {
+    // get the name and value from the form inputs
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
+
+    // update the state for the form (take the previous state, add it to a new object,
+    // add the input's name and value to a new object, return the new object, and use
+    // it to update the form state)
+    setFormInputs(prevState => ({...prevState, [inputName]:inputValue}));
+  };
+
+  // ---------- JSX to return ----------
   return (
     <div>
       <p>Login form</p>
@@ -6,13 +25,15 @@ function LoginForm() {
       {/* ---------- Form ---------- */}
       <form>
         <label>Username</label>
-        <input />
+        <input type="text" id="username" name="username" value={formInputs.username || ""} onChange={handleInputs} />
+
         <label>Password</label>
-        <input />
+        <input type="text" id="password" name="password" value={formInputs.password || ""} onChange={handleInputs} />
+
         <button>Log in</button>
       </form>
     </div>
   );
-}
+};
 
 export default LoginForm;
