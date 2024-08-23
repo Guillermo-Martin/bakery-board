bd6aef3891674549110cbea65adda6834119c3ed
Guillermo Martin <gscalica@gmail.com>
2024-08-23 16:27:52 -0700

    Add notes for future API route setup

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+
 // API routes
 // =============================================
 app.get('/', (req, res) => {
   res.send("Hello world");
 });
+
+
+// Example route setup and how to get information from the url
+// app.get('/example/:userId/books/:bookId', (req, res) => {
+//   console.log(req.params);
+//   res.send(req.params);
+// });
 
 // Listener
 // =============================================
