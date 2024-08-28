871fec329a58d4788f75cc1a19824cd836275c73
Guillermo Martin <gscalica@gmail.com>
2024-08-28 13:19:39 -0700

    Edit 'findUser' function in 'userController' to query the database for all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,22 @@
-function findUser(){
+// require User model
+const { User } = require("./../models/userSchema");
+
+function findUser(req, res){
   console.log("you're in the findUser function!");
+
+  User.find()
+    .then(user => {
+      res.json(user);
+      console.log(user, "line 9");
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 // export function to use in api routes
 module.exports = { findUser };
+
+// Todos:
+// 1.  have the function query the database for all users (temporarily)
+// 2.  require the model to use
