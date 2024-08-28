c1e55a4ab0755cb1526de25955b0932a8f94e222
Guillermo Martin <gscalica@gmail.com>
2024-08-28 11:34:55 -0700

    Modularize the models for the sample 'Kitten' model

--- controllers/kittenController.js
+++ controllers/kittenController.js
@@ -1,26 +1,19 @@
-// 1. require mongoose
-const mongoose = require("mongoose");
+// require model to use
+const { Kitten } = require("./../models/kittySchema");
 
-// 2. create the schema (the blue print of how the data should look in the database)
-const kittySchema = new mongoose.Schema({
-  name: String
-});
-
-// 3. compile the schema into a model (used to construct documents in the database) - name it and use the schema
-const Kitten = mongoose.model('Kitten', kittySchema);
-
-
-
+// function to get all kittens
 function getAllKittens(req, res){
   console.log("getAllKittens function");
 
-  const kittens = Kitten.find()
-  .then(kitten => {
-    res.json(kitten);
-    console.log(kitten, "line 44 ~~~~~~~~~~")
-  })
-  
-  console.log(kittens, "line 42~~~~~~~");
+  Kitten.find()
+    .then(kitten => {
+      res.json(kitten);
+      console.log(kitten, "line 44 ~~~~~~~~~~")
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
+// export function to use in api routes
 module.exports = { getAllKittens };
---
+++ models/kittySchema.js
@@ -0,0 +1,14 @@
+// 1. require mongoose
+const mongoose = require("mongoose");
+
+// 2. create the schema (the blueprint of how the data should look in the database)
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+// 3. compile the schema into a model (used to construct documents in the database)
+// - 1st argument: name of model; 2nd argument: blueprint to use
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+// 4. export model to use in its controller
+module.exports = { Kitten };
