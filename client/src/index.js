fa6f15e77923a1b5371a47145570a727652e5674
Guillermo Martin <gscalica@gmail.com>
2024-09-16 16:25:36 -0700

    Provide entire App component access to the Redux Store

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// import Redux store
+import store from "./store";
+import { Provider } from "react-redux";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// render App
 root.render(
   <React.StrictMode>
-    <App />
+    {/* ----- Give entire App access to the Redux store ----- */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
